cfd8885e68cb27c55daaedebe4d7ec7e73a045bb
IamYan94 <yanyanyan623@gmail.com>
2023-05-25 21:46:36 +0300

    InternetShopOperations

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import View.Impl.LoginMenu;
 public class Main {
     public static void main(String[] args) {
 
-        User oleh = UserManager.createClient("Oleh", "Sakharuk", "oleh.sahar@.com", "000", 5500.00);
+
+
+        User oleh = UserManager.createClient("Oleg", "Sakharuk", "oleh.sahar@.com", "000", 5500.00);
+
         User natalia = UserManager.createClient("Natalia", "Nikolaieva", "natalia.skoropad@.com", "0210", 3100.00);
         User nastia = UserManager.createClient("Nastia", "Sakharuk", "nastyashurba.com", "1234", 12750.00);
 
